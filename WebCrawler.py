f86d394439980776f5320c9667ce3bb3b92ac4ae
Daniel Flockhart <57047926+DanielFlockhart@users.noreply.github.com>
2020-12-14 23:42:52 +0000

    Delete WebCrawler.py

--- WebCrawler.py
+++
@@ -1,9 +0,0 @@
-import wikipedia
-print(wikipedia.search("Programming"))
-
-wiki_wiki = wikipedia.Wikipedia('en')
-
-page_py = wiki_wiki.page('Python_(programming_language)')
-print(p_wiki.text)
-
-
